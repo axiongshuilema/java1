fd42eb42f9acdd0ffbb5ac5237b79a4d9795d370
啊熊 <axiongshuilema@163.com>
2022-10-28 02:48:08 +0800

    我的第一个Java程序

---
+++ java-foundatinal/src/main/java/net/ittimeline/java/foundational/object/HelloWorld.java
@@ -0,0 +1,14 @@
+package net.ittimeline.java.foundational.object;
+
+/**
+ * 我的多一个Java程序
+ *
+ * @author tony axiongshuilema@163.com
+ * @version 2022/10/28 02:36
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World with mac $ JDK11 & Git & Maven&Intellij IDEA 2022.2.3");
+    }
+}
